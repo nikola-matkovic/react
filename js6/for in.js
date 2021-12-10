685eaef0093909e79d97705a614595d05a9d250d
anonymousvegan <majjunjulavgust@gmail.com>
2021-12-10 17:16:13 +0100

    learning es6 loops

---
+++ js6/for in.js
@@ -0,0 +1,6 @@
+//for ...in loop
+const person = {fName:"nikola", lName:"matkovic", age:19}
+
+for(let key in person){
+    console.log(person[key])
+}
---
+++ js6/for of.js
@@ -0,0 +1,6 @@
+// for (var  of iterable
+let fname = "nikola";
+
+for(l of fname){
+    console.log(l);
+}
