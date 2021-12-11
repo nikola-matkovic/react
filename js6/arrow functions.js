53bce596e654c375afe0b238a9c60db0b96a4be7
anonymousvegan <majjunjulavgust@gmail.com>
2021-12-11 18:50:06 +0100

    for off

--- js6/arrow functions.js
+++ js6/arrow functions.js
@@ -3,7 +3,17 @@ function test(){
 }
 test();
 
+let hello = () => "hello world"; //return by default
+console.log(hello());
+
 var a = () => {
     console.log("test2")
 }
 a();
+
+//if you have only one parameter in arrow function, you can skip the parentheses
+
+
+hello = val => "Hello " + val;
+
+console.log(hello("Nikola"));
--- js6/for of.js
+++ js6/for of.js
@@ -1,6 +1,25 @@
 // for (var  of iterable
 let fname = "nikola";
 
-for(l of fname){
-    console.log(l);
+for(let i of fname){
+    console.log(i);
 }
+
+const  arr = [1, 2, 3, 4, 5];
+
+for(let i  of arr){
+    console.log(i);
+}
+
+const map = new Map();
+map.set("fName", "nikola");
+map.set("lName", "matkovic");
+map.set("age", 19);
+
+//vraća sve elemente sa sve ključem
+for(let i of map){
+    console.log(i);
+}
+map.forEach((val, key)=>{
+    console.log(`Key: ${key} \t value: ${val} \n`);
+});
