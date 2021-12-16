eed817e58413ef1e4e1bd47a5dd52f79e5b7d58c
anonymousvegan <majjunjulavgust@gmail.com>
2021-12-16 20:35:57 +0100

    finished maps

--- js6/map.js
+++ js6/map.js
@@ -1,4 +1,22 @@
 //
-const m1 = new map([
-    [""]
+'use strict'
+const m1 = new Map([                             //new map
+    [1, "one"],
+    [2, "two"],
+    [3, "three"]
 ]);
+process.stdout.write(m1.get(2));
+m1.set(4, "four");                              //set
+m1.delete(1);                                   //delete
+if(m1.has(3)){                                  //has
+    console.log("number 3 is in the map");
+}
+m1.forEach((val) => {                          //foreach
+    console.log(val);
+});
+let s = m1.size;
+console.log("with iter:");
+const iter = m1.entries();
+for(let i=0; i<=s; i++){
+    console.log(iter.next());
+}
