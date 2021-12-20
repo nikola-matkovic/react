1a2b734a15ddbb9dbcd6346339ceb70730e5cfb5
anonymousvegan <majjunjulavgust@gmail.com>
2021-12-20 22:53:49 +0100

    basics

--- js6/classes.js
+++ js6/classes.js
@@ -3,6 +3,17 @@ class Person {
     this.fName = fName;
     this.lName = lName;
     this.age = age;
+  }
+  fullName(){
+    return this.fName + " " +this.lName;
+  }
+}
+class Looking extends Person {
+  cnstructor(eyes, height){
     
   }
 }
+
+
+const p1 = new Person("Nikola", "Matković", 19);
+console.log(p1.fullName());
