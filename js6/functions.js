d3e0eeac83cbb16c67b2fcebcad4fac2e8674516
anonymousvegan <majjunjulavgust@gmail.com>
2021-12-20 21:58:03 +0100

    functions pro

--- js6/functions.js
+++ js6/functions.js
@@ -4,3 +4,20 @@ function Person(first, last, age){ //constructor  for object
   this.lastName  = last;
   this.age  = age;
 }
+
+function write(){
+  console.log("hello world");
+}
+
+max = findMax(1, 2, 3, 4, 5, 10, 8, 9);
+
+function findMax(){
+  let max = -Infinity;
+  for(key in arguments){
+    if(arguments[key] > max){
+      max = arguments[key];
+    }
+  }
+  return max;
+}
+console.log(max);
