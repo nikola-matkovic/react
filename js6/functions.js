c334cd56f47edc3d3b29289b1bab27321e0b95a4
anonymousvegan <majjunjulavgust@gmail.com>
2021-12-20 22:16:41 +0100

    learning functions

--- js6/functions.js
+++ js6/functions.js
@@ -4,6 +4,8 @@ function Person(first, last, age){ //constructor  for object
   this.lastName  = last;
   this.age  = age;
 }
+person1 = new Person("Jovan", "Jovanović", 25);
+
 
 function write(){
   console.log("hello world");
@@ -21,3 +23,37 @@ function findMax(){
   return max;
 }
 console.log(max);
+//call()
+const phone ={
+  turnOn: function (){
+    console.log(`i'm ${this.name} ${this.model} and i'm On!`);
+  }
+}
+const  phone1 = {
+  name: "xiaomi",
+  model: "mi 5x",
+  ram: "4GB",
+  rom: "32GB"
+}
+phone.turnOn.call(phone1);
+
+const laptop ={
+  turnOn: function (battery, time){
+    console.log(`i'm ${this.name} ${this.model} and i'm On. i needed ${time}secounds for boot. Now i  have ${battery}% of battery`);
+  }
+}
+const laptop1 = {
+  name: "lenovo",
+  model: "thinkpad",
+  ram: "8GB",
+  rom: "250GB"
+}
+const laptop2 = {
+  name: "apple",
+  model: "M1",
+  ram: "8GB",
+  rom: "250GB"
+}
+laptop.turnOn.call(laptop1, 50, 90);
+laptop.turnOn.apply(laptop2, [12, 40]);
+
