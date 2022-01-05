587368f32f13f1978ea831fc399e0609d3a18804
anonymousvegan <majjunjulavgust@gmail.com>
2022-01-05 12:55:19 +0100

    something new

--- js6/listBinding.js
+++ js6/listBinding.js
@@ -3,19 +3,15 @@ class  ListBinding{
         this.listElement = element;
         this.textList = ["Apple", "Banana", "Orange"];
     }
-
-    //make an <li> element 
     static createListItem(text){
         const li = document.createElement("li");
         li.textContent = text;
         return li;
     }
     update(){
-        /* Remove all existling li elements  */
         while(this.listElement.firstChild){
             this.listElement.removeChild(this.listElement.firstChild);
         }
-        // FIll list with  <li> tags
         this.textList.forEach((text) =>
             this.listElement.appendChild(ListBinding.createListItem(text))
         );
@@ -31,12 +27,9 @@ class  ListBinding{
                 this.textList.splice(this.textList.indexOf(text), 1);
                 this.update();
             }
-            else{
+            else
                 throw "Nije pronađen element sa tim imenom! "
-            }
         }
-        catch (err) {
-            console.log(err);
-        }
+        catch (err) { console.log(err); }
     }
 }
