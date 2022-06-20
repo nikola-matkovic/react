216a7671124505e913bccd666273cae887825469
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-20 22:05:04 +0200

    created new index.js with new skills

---
+++ druga-aplikacija/index.old.js
@@ -0,0 +1,68 @@
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                    </a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a className="dropdown-item" href="#">Action</a>
+                    <a className="dropdown-item" href="#">Another action</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" href="#">Disabled</a>
+                </li>
+                </ul>
+                <form className="form-inline my-2 my-lg-0">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    )
+}
+
+function Main() {
+    return (
+    <div>
+        <h1>Hello, React!</h1>
+        <p>Fruits</p>
+        <ul>
+            <li>Banana</li>
+            <li>Orange</li>
+            <li>Lemon</li>
+            <li>apple</li>
+        </ul>
+    </div >)
+}
+
+const root = document.querySelector("#root")
+ReactDOM.render(
+    <div>
+        <Navbar />
+        <Main />
+    </div >
+,root)
+
+
+// more  complicated with standard way!
+
+// let h1 = document.createElement("H1")
+// h1.textContent = "Hello, React!"
+// root.appendChild(h1);
