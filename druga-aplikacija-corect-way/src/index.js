ca2ff633ecfd5eb7396fa85e2af8090689ac8fef
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-21 00:11:18 +0200

    some small changes

--- druga-aplikacija-corect-way/src/index.js
+++ druga-aplikacija-corect-way/src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Page = 
+    <div>
+      <h1>test1</h1>
+      <h2>test2</h2>
+      <h3>test3</h3>
+      <h4>test4</h4>
+      <h5>test5</h5>
+    </div>
+
+
+
+const rootElem = document.getElementById("root");
+
+rootElem.append(JSON.stringify(Page))
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Page/>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
