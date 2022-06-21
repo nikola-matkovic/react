8e24848dd2c0504f45e80d9d13c7d2bdd267fd37
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-21 21:09:30 +0200

    making rist reals site

--- druga-aplikacija-corect-way/src/index.js
+++ druga-aplikacija-corect-way/src/index.js
@@ -1,32 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const Page = 
-    <div>
-      <h1>test1</h1>
-      <h2>test2</h2>
-      <h3>test3</h3>
-      <h4>test4</h4>
-      <h5>test5</h5>
-    </div>
+const page = (
+  <div>
+    <h1>My awesome website in react</h1>
+    <h3>Reasons I love React</h3>
+    <ol>
+      <li>It's composable</li>
+      <li>It's declarative</li>
+      <li>it's hirable skill</li>
+      <li>it's actively maintained by skilled people</li>
+    </ol>
+  </div>
+)
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <page />
+  </React.StrictMode>
+);
 
-
-const rootElem = document.getElementById("root");
-
-rootElem.append(JSON.stringify(Page))
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <Page/>
-//   </React.StrictMode>
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
