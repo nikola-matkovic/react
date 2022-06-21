02d0388833925f0204daab7ec123bcf5747b36df
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-21 21:24:04 +0200

    added logo and some text

--- druga-aplikacija-corect-way/src/index.js
+++ druga-aplikacija-corect-way/src/index.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const page = (
-  <div>
-    <h1>My awesome website in react</h1>
-    <h3>Reasons I love React</h3>
-    <ol>
-      <li>It's composable</li>
-      <li>It's declarative</li>
-      <li>it's hirable skill</li>
-      <li>it's actively maintained by skilled people</li>
-    </ol>
-  </div>
-)
+function Page() {
+  return (
+    <div>
+      <img src="./logo192.png" alt="React Logo" />
+      <h1>Fun fact about react</h1>
+      <ul>
+        <li>Was first relased in 2013</li>
+        <li>Was originally created by Jordan Walks</li>
+        <li>Has over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>powewrs thousends of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <page />
+    <Page />
   </React.StrictMode>
 );
 
