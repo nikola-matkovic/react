c8d4078b2d61e39dad7c71407912a6323c44ea6c
anonymousvegan <majjunjulavgust@gmail.com>
2022-07-11 23:18:44 +0200

    fragmet changed to div

--- druga-aplikacija-correct-way/src/App.jsx
+++ druga-aplikacija-correct-way/src/App.jsx
@@ -4,10 +4,10 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 function App() {
   return ( 
-    <>
+    <div>
       <Header />
       <MainContent />
-    </>
+    </div>
   );
 }
 
