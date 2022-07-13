90eae8035fe7a7e6c28a511086f25031ff3d9af8
anonymousvegan <majjunjulavgust@gmail.com>
2022-07-13 20:27:23 +0200

    hero image fixed

--- airbnb-clone/src/components/hero/hero.jsx
+++ airbnb-clone/src/components/hero/hero.jsx
@@ -3,7 +3,7 @@ import group from '../../images/group.png';
 export default function Hero() { 
     return (
         <nav className={style.hero}> 
-            <img src={group} alt="" />
+            <img className={style.group} src={group} alt="" />
             <h2 className={style.title}>Online Experiences</h2>
             <p className={style.text}>
                 Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
