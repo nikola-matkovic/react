360646793ecb80598d19571982011c51a20677cc
anonymousvegan <majjunjulavgust@gmail.com>
2022-07-14 14:01:00 +0200

    added audio tag

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,11 +1,15 @@
 import React, {useState} from 'react';
-
+import pitanja from './files/pitanja'
 import style from './App.module.css';
+import Audio from './components/Audio'
 
 function App() {
   return (
     <div className={style.container}>
-      
+      <Audio />
+      <Kraj />
+      <Pitanje />
+      <Odgovori />
     </div>
   );
 }
