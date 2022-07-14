24199627f94ab227aa701a4c71a6d1b91ea5d9a6
anonymousvegan <majjunjulavgust@gmail.com>
2022-07-14 14:08:21 +0200

    added new comonents

---
+++ quiz-app/src/components/Audio/Audio.jsx
@@ -0,0 +1,9 @@
+import style from 'audio.module.css'
+
+const Audio = (props) => {
+    return (
+        <audio id="audio"></audio>
+    )
+}
+
+export default Audio;
---
+++ quiz-app/src/components/Kraj/Kraj.jsx
@@ -0,0 +1,16 @@
+import style from 'kraj.module.css'
+
+const Kraj = (props) => {
+    return (
+        <div className={style.kraj}>
+            <div id="krajnji-tekst"></div>
+            <div id="rezultat">
+                <p>Tvoj  rezultat je: <br />
+                    <span id="rezultat-broj">5/10</span>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Kraj;
