14344a2ad2e70f65b889a2350c42d8979471db92
anonymousvegan <majjunjulavgust@gmail.com>
2022-07-14 13:12:24 +0200

    making template for quiz game

---
+++ quiz-app/src/App.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+import style from './App.module.css';
+
+function App() {
+  return (
+    <div className={style.container}>
+      
+    </div>
+  );
+}
+
+export default App;
