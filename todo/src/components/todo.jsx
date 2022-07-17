08b8336970878c726742d7bf36b94cbe6981d06a
anonymousvegan <majjunjulavgust@gmail.com>
2022-07-17 13:08:42 +0200

    test

--- todo/src/components/todo.jsx
+++ todo/src/components/todo.jsx
@@ -1,17 +1,27 @@
 import style from './style.module.css';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Todo = () => {
+    const handleInputChange = (e)  => {
+        setValue(e.target.value);
+    }
+    const [value, setValue] = useState('');
     return (
         <div className={style.wrapper}>
             <header>Todo App</header>
             <div className={style.inputField}>
-                <input type="text" placeholder="Add your new todo" />
-                <button><i class="fas fa-plus"></i></button>
+                <input
+                    type="text"
+                    placeholder="Add your new todo"
+                    value={value} 
+                    onChange={(e) => handleInputChange(e)}/>
+                <button><FontAwesomeIcon icon="plus"></FontAwesomeIcon></button>
             </div>
             <ul className={style.todoList}>
             </ul>
             <div className={style.footer}>
-            <span>You have <span class="pendingTasks"></span> pending tasks</span>
+            <span>You have <span className="pendingTasks"></span> pending tasks</span>
             <button>Clear All</button>
             </div>
         </div>
