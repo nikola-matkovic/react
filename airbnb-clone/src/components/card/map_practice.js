de71f5146a7cdd654780270d9dbc9f1f585ae09a
anonymousvegan <majjunjulavgust@gmail.com>
2022-07-18 09:43:58 +0200

    map practice

---
+++ airbnb-clone/src/components/card/map_practice.js
@@ -0,0 +1,7 @@
+let a1 = [1,2,3,4,5]
+
+console.log(a1.map(number=> number**2));
+
+let a2 = ["Alice", "bob", "charlie", "danielle"]
+
+console.log(a2.map(name => name[0].toUpperCase() + name.slice(1).toLowerCase() ));
