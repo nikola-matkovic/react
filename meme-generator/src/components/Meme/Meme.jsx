574c1e8a4014c51119e0d2863bccbdaee6e66eb4
anonymousvegan <majjunjulavgust@gmail.com>
2022-07-20 16:20:24 +0200

    added state

--- meme-generator/src/components/Meme/Meme.jsx
+++ meme-generator/src/components/Meme/Meme.jsx
@@ -1,12 +1,15 @@
 import style from './style.module.css';
 import memes from './memes.json';
+import { useState } from 'react';
 
 const Meme = (props) => {
-    const handleButtonClick = (e) => {
+    const [meme, setMeme] = useState({topText: "", bottomText: "", url: "https://i.imgflip.com/1tl71a.jpg"});
+    const [allMemeImages, setAllMemeImages] = useState(memes);
+    const getMeme = () => {
         let memesData = memes.data.memes;
         let randomNumber = Math.floor(Math.random() * memesData.length);
         let url = memesData[randomNumber].url;
-        console.log(url);
+        setMeme( (prevState) =>({...prevState, url}));
     }
     return (
         <main className={style.memeContainer}>
@@ -19,8 +22,9 @@ const Meme = (props) => {
                     <input type="text" placeholder='top text'/>
                     <input type="text" placeholder='bottom text'/>
                 </div>
-                <button type="submit" onClick={(e) => handleButtonClick(e)}>Get a new meme image 🖼️</button>
+                <button type="submit" onClick={(e) => getMeme()}>Get a new meme image 🖼️</button>
             </form>
+            <img src={meme.url} alt="meme-image" />
         </main>
     )
 }
