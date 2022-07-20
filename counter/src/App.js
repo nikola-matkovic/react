6ba0333d431e7618f233732fb431145161d56d5c
anonymousvegan <majjunjulavgust@gmail.com>
2022-07-20 13:30:23 +0200

    arrow funciuton in state setter

--- counter/src/App.js
+++ counter/src/App.js
@@ -8,8 +8,8 @@ function App() {
       <div className={style.counter}>
         <div className={style.number}>
           {number}
-          <div className={style.plus} onClick={()=> setNumber(number-1)}>-</div>
-          <div className={style.minus} onClick={()=> setNumber(number+1)}>+</div>
+          <div className={style.plus} onClick={()=> setNumber((prevCount => prevCount + 1))}>-</div>
+          <div className={style.minus} onClick={()=> setNumber( (prevCount) => prevCount + 1)}>+</div>
         </div>
       </div>
     </div>
