e177e38723fd284d302850e411a074303abf5978
anonymousvegan <majjunjulavgust@gmail.com>
2022-07-22 19:41:27 +0200

    it works now

--- nit course/src/index.js
+++ nit course/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import MainButton from "./components/functional/MainButton/index";
+import Checkbox from "./components/functional/Checkbox/index";
 
 import "./index.css";
 
@@ -9,5 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode> 
     <MainButton text="klikni me"  color="danger" />
+    <Checkbox text="klikni me"  color="danger" />
   </React.StrictMode>
 );
