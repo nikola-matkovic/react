9e2645a580b86e6716cb2e64d4bb6cb06fbe5e57
anonymousvegan <majjunjulavgust@gmail.com>
2022-07-22 20:04:51 +0200

    checkbox finished

--- nit course/src/components/functional/Checkbox/index.jsx
+++ nit course/src/components/functional/Checkbox/index.jsx
@@ -1,10 +1,17 @@
 import style from './style.module.css';
 const Checkbox = (props) => {
-    const {text = "Slažem se da prodajem bubreg", color="primary", size={md}} = props;
+    let {text = "Slažem se da prodajem bubreg", color, size, radius} = props;
     let colors = ["primary", "secondary", "success", "danger", "warning", "info", "light", "dark"];
-    let sizes = ["sm", "md", "lg", "xl", ];
+    let sizes = ["xs", "sm", "md", "lg", "xl", ];
+    let radiuses = ["xsradius", "smradius", "mdradius", "lgradius", "xlradius"];
+    color = colors.includes(color) ? color : "primary";
+    size = sizes.includes(size) ? size : "md";
+    radius = radiuses.includes(radius) ? radius : "mdradius";
     return (
-        <label>{text}</label>
+        <>
+            <input type='checkbox' id='check' className={`${style.checkbox} ${style[color]} ${style[size]} ${style[radius]}`}/>
+            <label htmlFor='check'>{text}</label>
+        </>
     );
 }
 
--- nit course/src/index.js
+++ nit course/src/index.js
@@ -1,20 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import MainButton from "./components/functional/MainButton/index";
 import Checkbox from "./components/functional/Checkbox/index";
-
-
 import "./index.css";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode> 
-<<<<<<< HEAD
-    <MainButton text="klikni me"  color="danger" />
-    <Checkbox text="klikni me"  color="danger" />
-=======
-    <MainButton text="klikni me"  color="danger" radius="xs" />
->>>>>>> aadbc2fb27283d2722764c6470c7abd88570399c
+  <React.StrictMode>
+    <Checkbox text="slažem se"  color="danger" />
   </React.StrictMode>
 );
