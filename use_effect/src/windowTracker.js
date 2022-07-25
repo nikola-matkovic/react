efaceb5fbddc63cca4b90007ecdbcff705eb2366
anonymousvegan <majjunjulavgust@gmail.com>
2022-07-25 13:50:47 +0200

    just cleaner code

--- use_effect/src/windowTracker.js
+++ use_effect/src/windowTracker.js
@@ -2,14 +2,11 @@ import React, {useEffect, useState} from "react"
 export default function WindowTracker() {
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
     useEffect ( () => {
-        const watchWidth = () => {
-            setWindowWidth(window.innerWidth)
-        }
+        const watchWidth = () => setWindowWidth(window.innerWidth)
         window.addEventListener('resize', watchWidth);
-        return () => {
-            window.removeEventListener('resize', watchWidth)
-        }
+        return () => window.removeEventListener('resize', watchWidth)
     }, [])
+    
     return (
         <h1>Window width: {windowWidth}</h1>
     )
