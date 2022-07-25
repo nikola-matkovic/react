6e8abf34ba29a687363ea00972eb2ee094fa7d17
anonymousvegan <majjunjulavgust@gmail.com>
2022-07-25 13:59:04 +0200

    async / await

--- meme-generator/src/components/Meme/Meme.jsx
+++ meme-generator/src/components/Meme/Meme.jsx
@@ -22,11 +22,12 @@ const Meme = (props) => {
     }
 
     useEffect( () => {
-        fetch("https://api.imgflip.com/get_memes")
-        .then( res => res.json())
-        .then(data => {
-           setAllMemeImages(data.data.memes);
-        }) 
+        async function getMemes () {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemeImages(data.data.memes);
+        }
+        getMemes()
     },[])
 
     return (
