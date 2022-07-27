2a631f3c71ab37939eeb04fdc34580cc9480ce1b
anonymousvegan <majjunjulavgust@gmail.com>
2022-07-27 22:43:58 +0200

    layout

---
+++ nit course/src/components/functional/Website/Layout/index.jsx
@@ -0,0 +1,12 @@
+import Nav from "../Nav";
+
+const Layout = (props) => {
+  return (
+    <div>
+      <Nav />
+      {props.children}
+      <Footer />
+    </div>
+  );
+};
+export default Layout;
