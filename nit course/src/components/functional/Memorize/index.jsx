e002c638851a4ebb1c5cfcbce5edc178d60c15cf
anonymousvegan <majjunjulavgust@gmail.com>
2022-08-13 18:10:06 +0200

    get inner text

--- nit course/src/components/functional/Memorize/index.jsx
+++ nit course/src/components/functional/Memorize/index.jsx
@@ -1,34 +1,35 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { memo, useState, useCallback } from "react";
+import { useEffect } from "react";
 
 const Memorize = (props) => {
   const [text, setText] = useState(1);
 
-  const change = useCallback( (e) => {
+  const click = useCallback( (e) => {
     setText(e.target.value)
-  }, [])
+    console.log(e.target.innerText);
+  }, [text])
 
   return (
     <div>
       <p>{text}</p>
-      <MyInput onChange = {change} ></MyInput>
-      <MyInput onChange = {change}></MyInput>
-      <MyInput onChange = {change}></MyInput>
-      <MyInput onChange = {change}></MyInput>
-      <MyInput onChange = {change}></MyInput>
-
+      <List />
     </div>
   );
 };
 
-const MyInput = React.memo((props) => {
+const MyInput = memo((props) => {
   return <div>
     <span>Nešto</span>
     <input type="text" onChange={props.onChange} />
   </div>;
-});
+});  
 
 const MyText = () => {
   return (<p>my text</p>)
 }
+
+const List = memo((props) => {
+  return [1,2,3, 4].map( item =>  <p>{item}</p>)
+})
 
 export default Memorize;
