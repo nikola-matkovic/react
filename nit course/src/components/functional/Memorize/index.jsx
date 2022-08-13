83d6b17094a1965aa80c6315c7197e5c1f43d370
anonymousvegan <majjunjulavgust@gmail.com>
2022-08-13 17:49:32 +0200

    use callback

--- nit course/src/components/functional/Memorize/index.jsx
+++ nit course/src/components/functional/Memorize/index.jsx
@@ -3,14 +3,19 @@ import React, { useEffect, useState, useCallback } from "react";
 const Memorize = (props) => {
   const [text, setText] = useState(1);
 
-  const change = (e) => {
-    console.log(e.target.value);
-  }
+  const change = useCallback( (e) => {
+    setText(e.target.value)
+  }, [])
 
   return (
     <div>
       <p>{text}</p>
-      <MyInput></MyInput>
+      <MyInput onChange = {change} ></MyInput>
+      <MyInput onChange = {change}></MyInput>
+      <MyInput onChange = {change}></MyInput>
+      <MyInput onChange = {change}></MyInput>
+      <MyInput onChange = {change}></MyInput>
+
     </div>
   );
 };
