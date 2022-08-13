c6131637717a6929e8d3f4ca24f03bbb4c48ccc5
anonymousvegan <majjunjulavgust@gmail.com>
2022-08-13 17:45:18 +0200

    use memo 2

--- nit course/src/components/functional/Memorize/index.jsx
+++ nit course/src/components/functional/Memorize/index.jsx
@@ -3,15 +3,27 @@ import React, { useEffect, useState, useCallback } from "react";
 const Memorize = (props) => {
   const [text, setText] = useState(1);
 
+  const change = (e) => {
+    console.log(e.target.value);
+  }
+
   return (
     <div>
       <p>{text}</p>
+      <MyInput></MyInput>
     </div>
   );
 };
 
 const MyInput = React.memo((props) => {
-  return <div></div>;
+  return <div>
+    <span>Nešto</span>
+    <input type="text" onChange={props.onChange} />
+  </div>;
 });
+
+const MyText = () => {
+  return (<p>my text</p>)
+}
 
 export default Memorize;
