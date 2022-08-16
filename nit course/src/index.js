833a757506641097fc731005c418fd5b6eb4477e
anonymousvegan <majjunjulavgust@gmail.com>
2022-08-16 08:43:56 -0700

    react path

--- nit course/src/index.js
+++ nit course/src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-
-import StateHook from "./components/hooks_learning/StateHook";
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
    <React.StrictMode>
-      <div>
-        <StateHook />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/team" element={<div>team</div>} />  
+          <Route path="/about" element={<div>about</div>} />
+          <Route path="/contact" element={<div>Contact</div>} />
+        </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 );
