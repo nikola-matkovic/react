079a45b885d6d7543960134732e2266114b14bdb
anonymousvegan <majjunjulavgust@gmail.com>
2022-08-19 18:58:34 +0200

    USE EFFECT

--- nit course/src/index.js
+++ nit course/src/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import RouterPage from "./components/functional/RouterPage";
-
+import EffectHook from "./components/hooks_learning/EffectHook";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
    <React.StrictMode>
-      <BrowserRouter>
-        <RouterPage>
-        </RouterPage>
-      </BrowserRouter>
+      <EffectHook />
   </React.StrictMode>
 );
