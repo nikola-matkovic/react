5b85d946c5d42d95daf45719ac78c50737291663
anonymousvegan <majjunjulavgust@gmail.com>
2022-08-22 13:40:00 +0200

    imporved styles

--- responsive-website/src/components/Footer/index.jsx
+++ responsive-website/src/components/Footer/index.jsx
@@ -1,7 +1,7 @@
 import style from "./style.module.css";
 const Footer = () => {
     return (
-        <footer className={style.Footer}>
+        <footer className={style.footer}>
             footer
         </footer> 
     );
